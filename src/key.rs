d5304113b9488839fa2e99b3ef935165ef6715e8
Raphael Bost <raphael_bost@alumni.brown.edu>
2020-10-19 15:04:00 +0200

    Keys had the wrong size

--- src/key.rs
+++ src/key.rs
@@ -7,7 +7,7 @@ use zeroize::{Zeroize, Zeroizing};
 #[derive(Zeroize)]
 #[zeroize(drop)]
 pub struct Key256 {
-    content: [u8; 64],
+    content: [u8; 32],
 }
 
 impl Key256 {
@@ -27,15 +27,8 @@ impl Key256 {
     where
         R: CryptoRng + RngCore,
     {
-        // we cannot call csprng.gen() on 64 elements arrays (it is limited to 32 elements)
-        // instead, create a zeroizing buffer and fill it with rng.fill_bytes
-        let mut randomness = Zeroizing::new([0u8; 64]);
-        csprng.fill_bytes(&mut *randomness);
-
-        let k = Key256 {
-            content: *randomness,
-        };
-
+        let mut k = Key256 { content: [0u8; 32] };
+        csprng.fill_bytes(&mut k.content);
         k
     }
 
@@ -56,12 +49,12 @@ impl Key256 {
     /// use crypto_tk_rs::Key256;
     ///
     /// /// Initializes a new key with bytes all set to `0x01`
-    /// let mut buf = [1u8;64];
+    /// let mut buf = [1u8;32];
     /// let k = Key256::from_bytes(&mut buf);
     /// /// buf is set to all 0
-    /// # assert_eq!(&buf[..], &[0u8; 64][..]);
+    /// # assert_eq!(&buf[..], &[0u8; 32][..]);
     /// ```
-    pub fn from_bytes(randomness: &mut [u8; 64]) -> Key256 {
+    pub fn from_bytes(randomness: &mut [u8; 32]) -> Key256 {
         let k = Key256 {
             content: *randomness,
         };
@@ -92,28 +85,22 @@ mod tests {
 
     #[test]
     fn build_from() {
-        let mut buf: [u8; 64] = [
+        let mut buf: [u8; 32] = [
             0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A,
             0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15,
-            0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F, 0x20,
-            0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2A, 0x2B,
-            0x2C, 0x2D, 0x2E, 0x2F, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36,
-            0x37, 0x38, 0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0x3E, 0x3F,
+            0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F,
         ];
 
-        let mut buf_copy = [0u8; 64];
+        let mut buf_copy = [0u8; 32];
         buf_copy.copy_from_slice(&buf);
 
         let k = Key256::from_bytes(&mut buf);
 
-        // The following comment was true prior Rust 1.47, but changed with the introduction of traits on arrays of any length.
-        // We cannot compare 64 elements arrays (the comparison works for at most
-        // 32 elements). We have to compare slices.
-        assert_eq!(&k.content[..], &buf_copy[..]);
+        assert_eq!(k.content, buf_copy);
 
-        assert_eq!(&k.content[..], &k.insecure_duplicate().content[..]);
+        assert_eq!(k.content, k.insecure_duplicate().content);
 
         // check that the buffer we built the key from has been cleared
-        assert_eq!(&buf[..], &[0u8; 64][..]);
+        assert_eq!(buf, [0u8; 32]);
     }
 }
