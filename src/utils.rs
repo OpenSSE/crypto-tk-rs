8cc75587186bcde3b0ea4fd2e06298112e077ab5
Raphael Bost <raphael_bost@alumni.brown.edu>
2022-01-14 23:14:09 +0100

    Remove the num-traits dependency

--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,23 @@
 //! Utility functions
 
-use num_traits::PrimInt;
 use std::convert::From;
 use std::ops::{Add, Div, Sub};
+
+/// Marker trait for primitive integers
+pub trait PrimitiveInt {}
+
+impl PrimitiveInt for u8 {}
+impl PrimitiveInt for u16 {}
+impl PrimitiveInt for u32 {}
+impl PrimitiveInt for u64 {}
+impl PrimitiveInt for u128 {}
+impl PrimitiveInt for usize {}
+impl PrimitiveInt for i8 {}
+impl PrimitiveInt for i16 {}
+impl PrimitiveInt for i32 {}
+impl PrimitiveInt for i64 {}
+impl PrimitiveInt for i128 {}
+impl PrimitiveInt for isize {}
 
 /// Compute ceil(x/y) when x and y are integer-like types
 ///
@@ -17,7 +32,11 @@ use std::ops::{Add, Div, Sub};
 /// ```
 pub fn int_ceil_div<T>(x: T, y: T) -> T
 where
-    T: Sub<Output = T> + Add<Output = T> + Div<Output = T> + From<u8> + PrimInt,
+    T: Sub<Output = T>
+        + Add<Output = T>
+        + Div<Output = T>
+        + From<u8>
+        + PrimitiveInt,
 {
     let one_1: T = std::convert::From::from(1u8);
     let one_2: T = std::convert::From::from(1u8);
